46f3577a39a5361ad9bc9a72efc8bb3b3fce2729
me-suzy <me.suzana@gmail.com>
2025-09-21 13:24:43 +0300

    Add Cursuri - Anul3 - FACUT WEB/FoxPro - Cursuri/1002 Things About Fox Pro/5/SOAPConst.h

---
+++ Cursuri - Anul3 - FACUT WEB/FoxPro - Cursuri/1002 Things About Fox Pro/5/SOAPConst.h
@@ -0,0 +1,128 @@
+
+*** Constant Group: smIsInputEnum
+#define smInput                                           -1
+#define smOutput                                          0
+#define smInOut                                           1
+
+*** Constant Group: __MIDL___MIDL_itf_mssoap_0114_0001
+#define smInput                                           -1
+#define smOutput                                          0
+#define smInOut                                           1
+
+*** Constant Group: enEncodingStyle
+#define enDocumentLiteral                                 0
+#define enDocumentEncoded                                 1
+#define enRPCLiteral                                      2
+#define enRPCEncoded                                      4
+
+*** Constant Group: __MIDL___MIDL_itf_mssoap_0114_0003
+#define enDocumentLiteral                                 0
+#define enDocumentEncoded                                 1
+#define enRPCLiteral                                      2
+#define enRPCEncoded                                      4
+
+*** Constant Group: enXSDType
+#define enXSDUndefined                                    -1
+#define enXSDDOM                                          0
+#define enXSDstring                                       1
+#define enXSDboolean                                      2
+#define enXSDfloat                                        3
+#define enXSDDouble                                       4
+#define enXSDdecimal                                      5
+#define enXSDtimeDuration                                 6
+#define enXSDrecurringDuration                            7
+#define enXSDbinary                                       8
+#define enXSDuriReference                                 9
+#define enXSDid                                           10
+#define enXSDidRef                                        11
+#define enXSDentity                                       12
+#define enXSDQName                                        13
+#define enXSDcdata                                        14
+#define enXSDtoken                                        15
+#define enXSDlanguage                                     16
+#define enXSDidRefs                                       17
+#define enXSDentities                                     18
+#define enXSDnmtoken                                      19
+#define enXSDnmtokens                                     20
+#define enXSDname                                         21
+#define enXSDncname                                       22
+#define enXSDnotation                                     23
+#define enXSDinteger                                      24
+#define enXSDnonpositiveInteger                           25
+#define enXSDlong                                         26
+#define enXSDint                                          27
+#define enXSDshort                                        28
+#define enXSDbyte                                         29
+#define enXSDnonNegativeInteger                           30
+#define enXSDnegativeInteger                              31
+#define enXSDunsignedLong                                 32
+#define enXSDunsignedInt                                  33
+#define enXSDunsignedShort                                34
+#define enXSDunsignedByte                                 35
+#define enXSDpositiveInteger                              36
+#define enXSDtimeInstant                                  37
+#define enXSDtime                                         38
+#define enXSDtimePeriod                                   39
+#define enXSDdate                                         40
+#define enXSDmonth                                        41
+#define enXSDyear                                         42
+#define enXSDcentury                                      43
+#define enXSDrecurringDate                                44
+#define enXSDrecurringDay                                 45
+#define enXSDarray                                        46
+#define enXSDanyType                                      47
+#define enTKempty                                         48
+#define enXSDEndOfBuildin                                 49
+
+*** Constant Group: __MIDL___MIDL_itf_mssoap_0114_0002
+#define enXSDUndefined                                    -1
+#define enXSDDOM                                          0
+#define enXSDstring                                       1
+#define enXSDboolean                                      2
+#define enXSDfloat                                        3
+#define enXSDDouble                                       4
+#define enXSDdecimal                                      5
+#define enXSDtimeDuration                                 6
+#define enXSDrecurringDuration                            7
+#define enXSDbinary                                       8
+#define enXSDuriReference                                 9
+#define enXSDid                                           10
+#define enXSDidRef                                        11
+#define enXSDentity                                       12
+#define enXSDQName                                        13
+#define enXSDcdata                                        14
+#define enXSDtoken                                        15
+#define enXSDlanguage                                     16
+#define enXSDidRefs                                       17
+#define enXSDentities                                     18
+#define enXSDnmtoken                                      19
+#define enXSDnmtokens                                     20
+#define enXSDname                                         21
+#define enXSDncname                                       22
+#define enXSDnotation                                     23
+#define enXSDinteger                                      24
+#define enXSDnonpositiveInteger                           25
+#define enXSDlong                                         26
+#define enXSDint                                          27
+#define enXSDshort                                        28
+#define enXSDbyte                                         29
+#define enXSDnonNegativeInteger                           30
+#define enXSDnegativeInteger                              31
+#define enXSDunsignedLong                                 32
+#define enXSDunsignedInt                                  33
+#define enXSDunsignedShort                                34
+#define enXSDunsignedByte                                 35
+#define enXSDpositiveInteger                              36
+#define enXSDtimeInstant                                  37
+#define enXSDtime                                         38
+#define enXSDtimePeriod                                   39
+#define enXSDdate                                         40
+#define enXSDmonth                                        41
+#define enXSDyear                                         42
+#define enXSDcentury                                      43
+#define enXSDrecurringDate                                44
+#define enXSDrecurringDay                                 45
+#define enXSDarray                                        46
+#define enXSDanyType                                      47
+#define enTKempty                                         48
+#define enXSDEndOfBuildin                                 49
