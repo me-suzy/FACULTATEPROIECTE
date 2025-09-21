ae4757cd1b0847740622b2dc82c6869b18749c98
me-suzy <me.suzana@gmail.com>
2025-09-21 13:50:19 +0300

    Add Cursuri - Anul3 - FACUT WEB/Java/De la Strambei/exemple/abstractizare/BusinessUnit.java

---
+++ Cursuri - Anul3 - FACUT WEB/Java/De la Strambei/exemple/abstractizare/BusinessUnit.java
@@ -0,0 +1,15 @@
+/*
+ * BusinessUnit.java
+ *
+ * Created on 24 martie 2003, 10:31
+ */
+
+package exemple.abstractizare;
+
+/**
+ *
+ * @author  strimbeic
+ */
+public abstract class BusinessUnit {
+    public abstract BusinessUnit getUnitateSuperioara();
+}
