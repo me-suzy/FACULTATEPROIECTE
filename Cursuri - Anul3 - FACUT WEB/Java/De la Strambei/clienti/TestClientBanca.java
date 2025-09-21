aeea02ace5b8c01a681b2bc625e0a695bc2139e9
me-suzy <me.suzana@gmail.com>
2025-09-21 13:48:56 +0300

    Add Cursuri - Anul3 - FACUT WEB/Java/De la Strambei/clienti/TestClientBanca.java

---
+++ Cursuri - Anul3 - FACUT WEB/Java/De la Strambei/clienti/TestClientBanca.java
@@ -0,0 +1,26 @@
+/*
+ * TestClientBanca.java
+ *
+ * Created on 24 martie 2004, 09:20
+ */
+
+package clienti;
+
+/**
+ *
+ * @author  strimbeic
+ */
+public class TestClientBanca {
+    
+    /** Creates a new instance of TestClientBanca */
+    public TestClientBanca() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
