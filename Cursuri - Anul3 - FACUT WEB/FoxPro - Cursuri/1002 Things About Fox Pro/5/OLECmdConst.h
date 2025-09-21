94dbd0a0a5b76d5da5d9240d301a583faad0f8f5
me-suzy <me.suzana@gmail.com>
2025-09-21 13:24:25 +0300

    Add Cursuri - Anul3 - FACUT WEB/FoxPro - Cursuri/1002 Things About Fox Pro/5/OLECmdConst.h

---
+++ Cursuri - Anul3 - FACUT WEB/FoxPro - Cursuri/1002 Things About Fox Pro/5/OLECmdConst.h
@@ -0,0 +1,96 @@
+
+*** Constant Group: CommandStateChangeConstants
+#define CSC_UPDATECOMMANDS                                -1
+#define CSC_NAVIGATEFORWARD                               1
+#define CSC_NAVIGATEBACK                                  2
+
+*** Constant Group: OLECMDID
+#define OLECMDID_OPEN                                     1
+#define OLECMDID_NEW                                      2
+#define OLECMDID_SAVE                                     3
+#define OLECMDID_SAVEAS                                   4
+#define OLECMDID_SAVECOPYAS                               5
+#define OLECMDID_PRINT                                    6
+#define OLECMDID_PRINTPREVIEW                             7
+#define OLECMDID_PAGESETUP                                8
+#define OLECMDID_SPELL                                    9
+#define OLECMDID_PROPERTIES                               10
+#define OLECMDID_CUT                                      11
+#define OLECMDID_COPY                                     12
+#define OLECMDID_PASTE                                    13
+#define OLECMDID_PASTESPECIAL                             14
+#define OLECMDID_UNDO                                     15
+#define OLECMDID_REDO                                     16
+#define OLECMDID_SELECTALL                                17
+#define OLECMDID_CLEARSELECTION                           18
+#define OLECMDID_ZOOM                                     19
+#define OLECMDID_GETZOOMRANGE                             20
+#define OLECMDID_UPDATECOMMANDS                           21
+#define OLECMDID_REFRESH                                  22
+#define OLECMDID_STOP                                     23
+#define OLECMDID_HIDETOOLBARS                             24
+#define OLECMDID_SETPROGRESSMAX                           25
+#define OLECMDID_SETPROGRESSPOS                           26
+#define OLECMDID_SETPROGRESSTEXT                          27
+#define OLECMDID_SETTITLE                                 28
+#define OLECMDID_SETDOWNLOADSTATE                         29
+#define OLECMDID_STOPDOWNLOAD                             30
+#define OLECMDID_ONTOOLBARACTIVATED                       31
+#define OLECMDID_FIND                                     32
+#define OLECMDID_DELETE                                   33
+#define OLECMDID_HTTPEQUIV                                34
+#define OLECMDID_HTTPEQUIV_DONE                           35
+#define OLECMDID_ENABLE_INTERACTION                       36
+#define OLECMDID_ONUNLOAD                                 37
+#define OLECMDID_PROPERTYBAG2                             38
+#define OLECMDID_PREREFRESH                               39
+#define OLECMDID_SHOWSCRIPTERROR                          40
+#define OLECMDID_SHOWMESSAGE                              41
+#define OLECMDID_SHOWFIND                                 42
+#define OLECMDID_SHOWPAGESETUP                            43
+#define OLECMDID_SHOWPRINT                                44
+#define OLECMDID_CLOSE                                    45
+#define OLECMDID_ALLOWUILESSSAVEAS                        46
+#define OLECMDID_DONTDOWNLOADCSS                          47
+#define OLECMDID_UPDATEPAGESTATUS                         48
+
+*** Constant Group: OLECMDF
+#define OLECMDF_SUPPORTED                                 1
+#define OLECMDF_ENABLED                                   2
+#define OLECMDF_LATCHED                                   4
+#define OLECMDF_NINCHED                                   8
+#define OLECMDF_INVISIBLE                                 16
+#define OLECMDF_DEFHIDEONCTXTMENU                         32
+
+*** Constant Group: OLECMDEXECOPT
+#define OLECMDEXECOPT_DODEFAULT                           0
+#define OLECMDEXECOPT_PROMPTUSER                          1
+#define OLECMDEXECOPT_DONTPROMPTUSER                      2
+#define OLECMDEXECOPT_SHOWHELP                            3
+
+*** Constant Group: tagREADYSTATE
+#define READYSTATE_UNINITIALIZED                          0
+#define READYSTATE_LOADING                                1
+#define READYSTATE_LOADED                                 2
+#define READYSTATE_INTERACTIVE                            3
+#define READYSTATE_COMPLETE                               4
+
+*** Constant Group: SecureLockIconConstants
+#define secureLockIconUnsecure                            0
+#define secureLockIconMixed                               1
+#define secureLockIconSecureUnknownBits                   2
+#define secureLockIconSecure40Bit                         3
+#define secureLockIconSecure56Bit                         4
+#define secureLockIconSecureFortezza                      5
+#define secureLockIconSecure128Bit                        6
+
+*** Constant Group: ShellWindowTypeConstants
+#define SWC_EXPLORER                                      0
+#define SWC_BROWSER                                       1
+#define SWC_3RDPARTY                                      2
+#define SWC_CALLBACK                                      4
+
+*** Constant Group: ShellWindowFindWindowOptions
+#define SWFO_NEEDDISPATCH                                 1
+#define SWFO_INCLUDEPENDING                               2
+#define SWFO_COOKIEPASSED                                 4
