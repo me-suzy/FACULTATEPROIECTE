15ec8db383a39c42abfa85e142443561620c5ba5
me-suzy <me.suzana@gmail.com>
2025-09-21 13:31:39 +0300

    Add Cursuri - Anul3 - FACUT WEB/FoxPro - Cursuri/1002 Things About Fox Pro/9/comctl32.h

---
+++ Cursuri - Anul3 - FACUT WEB/FoxPro - Cursuri/1002 Things About Fox Pro/9/comctl32.h
@@ -0,0 +1,217 @@
+*** Constant Group: BorderStyleConstants
+#define ccNone                                            0
+#define ccFixedSingle                                     1
+
+*** Constant Group: MousePointerConstants
+#define ccDefault                                         0
+#define ccArrow                                           1
+#define ccCross                                           2
+#define ccIBeam                                           3
+#define ccIcon                                            4
+#define ccSize                                            5
+#define ccSizeNESW                                        6
+#define ccSizeNS                                          7
+#define ccSizeNWSE                                        8
+#define ccSizeEW                                          9
+#define ccUpArrow                                         10
+#define ccHourglass                                       11
+#define ccNoDrop                                          12
+#define ccArrowHourglass                                  13
+#define ccArrowQuestion                                   14
+#define ccSizeAll                                         15
+#define ccCustom                                          99
+
+*** Constant Group: AppearanceConstants
+#define ccFlat                                            0
+#define cc3D                                              1
+
+
+*** Constant Group: OLEDragConstants
+#define ccOLEDragManual                                   0
+#define ccOLEDragAutomatic                                1
+
+*** Constant Group: OLEDropConstants
+#define ccOLEDropNone                                     0
+#define ccOLEDropManual                                   1
+
+*** Constant Group: DragOverConstants
+#define ccEnter                                           0
+#define ccLeave                                           1
+#define ccOver                                            2
+
+*** Constant Group: ClipBoardConstants
+#define ccCFText                                          1
+#define ccCFBitmap                                        2
+#define ccCFMetafile                                      3
+#define ccCFDIB                                           8
+#define ccCFPalette                                       9
+#define ccCFEMetafile                                     14
+#define ccCFFiles                                         15
+#define ccCFRTF                                           -16639
+
+*** Constant Group: OLEDropEffectConstants
+#define ccOLEDropEffectNone                               0
+#define ccOLEDropEffectCopy                               1
+#define ccOLEDropEffectMove                               2
+#define ccOLEDropEffectScroll                             -2147483648
+
+*** Constant Group: ErrorConstants
+#define ccInvalidProcedureCall                            5
+#define ccOutOfMemory                                     7
+#define ccTypeMismatch                                    13
+#define ccObjectVariableNotSet                            91
+#define ccInvalidPropertyValue                            380
+#define ccSetNotSupportedAtRuntime                        382
+#define ccSetNotSupported                                 383
+#define ccSetNotPermitted                                 387
+#define ccGetNotSupported                                 394
+#define ccInvalidPicture                                  481
+#define ccInvalidObjectUse                                425
+#define ccWrongClipboardFormat                            461
+#define ccDataObjectLocked                                672
+#define ccExpectedAnArgument                              673
+#define ccRecursiveOleDrag                                674
+#define ccFormatNotByteArray                              675
+#define ccDataNotSetForFormat                             676
+#define ccIndexOutOfBounds                                35600
+#define ccElemNotFound                                    35601
+#define ccNonUniqueKey                                    35602
+#define ccInvalidKey                                      35603
+#define ccElemNotPartOfCollection                         35605
+#define ccCollectionChangedDuringEnum                     35606
+#define ccWouldIntroduceCycle                             35614
+#define ccMissingRequiredArg                              35607
+#define ccBadObjectReference                              35610
+#define ccCircularReference                               35700
+#define ccCol1MustBeLeftAligned                           35604
+#define ccReadOnlyIfHasImages                             35611
+#define ccImageListMustBeInitialized                      35613
+#define ccNotSameSize                                     35615
+#define ccImageListLocked                                 35617
+#define ccOverlayParamTooLarge                            35618
+#define ccMaxPanelsExceeded                               35616
+#define ccMaxButtonsExceeded                              35619
+#define ccInvalidSafeModeProcCall                         680
+
+*** Constant Group: TabWidthStyleConstants
+#define tabJustified                                      0
+#define tabNonJustified                                   1
+#define tabFixed                                          2
+
+*** Constant Group: TabStyleConstants
+#define tabTabs                                           0
+#define tabButtons                                        1
+
+*** Constant Group: ButtonStyleConstants
+#define tbrDefault                                        0
+#define tbrCheck                                          1
+#define tbrButtonGroup                                    2
+#define tbrSeparator                                      3
+#define tbrPlaceholder                                    4
+
+*** Constant Group: ValueConstants
+#define tbrUnpressed                                      0
+#define tbrPressed                                        1
+
+*** Constant Group: SbarStyleConstants
+#define sbrNormal                                         0
+#define sbrSimple                                         1
+
+*** Constant Group: PanelAlignmentConstants
+#define sbrLeft                                           0
+#define sbrCenter                                         1
+#define sbrRight                                          2
+
+*** Constant Group: PanelAutoSizeConstants
+#define sbrNoAutoSize                                     0
+#define sbrSpring                                         1
+#define sbrContents                                       2
+
+*** Constant Group: PanelBevelConstants
+#define sbrNoBevel                                        0
+#define sbrInset                                          1
+#define sbrRaised                                         2
+
+*** Constant Group: PanelStyleConstants
+#define sbrText                                           0
+#define sbrCaps                                           1
+#define sbrNum                                            2
+#define sbrIns                                            3
+#define sbrScrl                                           4
+#define sbrTime                                           5
+#define sbrDate                                           6
+#define sbrKana                                           7
+
+*** Constant Group: LabelEditConstants
+#define tvwAutomatic                                      0
+#define tvwManual                                         1
+
+*** Constant Group: TreeLineStyleConstants
+#define tvwTreeLines                                      0
+#define tvwRootLines                                      1
+
+*** Constant Group: TreeStyleConstants
+#define tvwTextOnly                                       0
+#define tvwPictureText                                    1
+#define tvwPlusMinusText                                  2
+#define tvwPlusPictureText                                3
+#define tvwTreelinesText                                  4
+#define tvwTreelinesPictureText                           5
+#define tvwTreelinesPlusMinusText                         6
+#define tvwTreelinesPlusMinusPictureText                  7
+
+*** Constant Group: TreeRelationshipConstants
+#define tvwFirst                                          0
+#define tvwLast                                           1
+#define tvwNext                                           2
+#define tvwPrevious                                       3
+#define tvwChild                                          4
+
+*** Constant Group: ListArrangeConstants
+#define lvwNone                                           0
+#define lvwAutoLeft                                       1
+#define lvwAutoTop                                        2
+
+*** Constant Group: ListLabelEditConstants
+#define lvwAutomatic                                      0
+#define lvwManual                                         1
+
+*** Constant Group: ListSortOrderConstants
+#define lvwAscending                                      0
+#define lvwDescending                                     1
+
+*** Constant Group: ListViewConstants
+#define lvwIcon                                           0
+#define lvwSmallIcon                                      1
+#define lvwList                                           2
+#define lvwReport                                         3
+
+*** Constant Group: ListColumnAlignmentConstants
+#define lvwColumnLeft                                     0
+#define lvwColumnRight                                    1
+#define lvwColumnCenter                                   2
+
+*** Constant Group: ListFindItemWhereConstants
+#define lvwText                                           0
+#define lvwSubItem                                        1
+#define lvwTag                                            2
+
+*** Constant Group: ListFindItemHowConstants
+#define lvwWhole                                          0
+#define lvwPartial                                        1
+
+*** Constant Group: ImageDrawConstants
+#define imlNormal                                         0
+#define imlTransparent                                    1
+#define imlSelected                                       2
+#define imlFocus                                          3
+
+*** Constant Group: OrientationConstants
+#define sldHorizontal                                     0
+#define sldVertical                                       1
+
+*** Constant Group: TickStyleConstants
+#define sldBottomRight                                    0
+#define sldTopLeft                                        1
+#define sldBoth                                           2
+#define sldNoTicks                                        3
