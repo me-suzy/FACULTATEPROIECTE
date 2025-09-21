3f20e0cc5197c5ecfb342d16fdb6ccdc04d50a68
me-suzy <me.suzana@gmail.com>
2025-09-21 13:35:05 +0300

    Add Cursuri - Anul3 - FACUT WEB/FoxPro - Cursuri/1002 Things About Fox Pro/9/mplayconst.h

---
+++ Cursuri - Anul3 - FACUT WEB/FoxPro - Cursuri/1002 Things About Fox Pro/9/mplayconst.h
@@ -0,0 +1,72 @@
+*** Constant Group: ReadyStateConstants
+#define amvUninitialized                                  0
+#define amvLoading                                        1
+#define amvInteractive                                    3
+#define amvComplete                                       4
+
+*** Constant Group: MPPlayStateConstants
+#define mpStopped                                         0
+#define mpPaused                                          1
+#define mpPlaying                                         2
+#define mpWaiting                                         3
+#define mpScanForward                                     4
+#define mpScanReverse                                     5
+#define mpClosed                                          6
+
+*** Constant Group: MPDisplaySizeConstants
+#define mpDefaultSize                                     0
+#define mpHalfSize                                        1
+#define mpDoubleSize                                      2
+#define mpFullScreen                                      3
+#define mpFitToSize                                       4
+#define mpOneSixteenthScreen                              5
+#define mpOneFourthScreen                                 6
+#define mpOneHalfScreen                                   7
+
+*** Constant Group: MPReadyStateConstants
+#define mpReadyStateUninitialized                         0
+#define mpReadyStateLoading                               1
+#define mpReadyStateInteractive                           3
+#define mpReadyStateComplete                              4
+
+*** Constant Group: MPDisplayModeConstants
+#define mpTime                                            0
+#define mpFrames                                          1
+
+*** Constant Group: MPMoreInfoType
+#define mpShowURL                                         0
+#define mpClipURL                                         1
+#define mpBannerURL                                       2
+
+*** Constant Group: MPMediaInfoType
+#define mpShowFilename                                    0
+#define mpShowTitle                                       1
+#define mpShowAuthor                                      2
+#define mpShowCopyright                                   3
+#define mpShowRating                                      4
+#define mpShowDescription                                 5
+#define mpShowLogoIcon                                    6
+#define mpClipFilename                                    7
+#define mpClipTitle                                       8
+#define mpClipAuthor                                      9
+#define mpClipCopyright                                   10
+#define mpClipRating                                      11
+#define mpClipDescription                                 12
+#define mpClipLogoIcon                                    13
+#define mpBannerImage                                     14
+#define mpBannerMoreInfo                                  15
+#define mpWatermark                                       16
+
+*** Constant Group: DVDMenuIDConstants
+#define dvdMenu_Title                                     2
+#define dvdMenu_Root                                      3
+#define dvdMenu_Subpicture                                4
+#define dvdMenu_Audio                                     5
+#define dvdMenu_Angle                                     6
+#define dvdMenu_Chapter                                   7
+
+*** Constant Group: MPShowDialogConstants
+#define mpShowDialogHelp                                  0
+#define mpShowDialogStatistics                            1
+#define mpShowDialogOptions                               2
+#define mpShowDialogContextMenu                           3
