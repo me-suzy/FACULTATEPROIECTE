06a3acf6b99ab367cc3fbcc74e2eb271ec7ef9f5
me-suzy <me.suzana@gmail.com>
2025-09-21 13:34:36 +0300

    Add Cursuri - Anul3 - FACUT WEB/FoxPro - Cursuri/1002 Things About Fox Pro/9/MCIConst.h

---
+++ Cursuri - Anul3 - FACUT WEB/FoxPro - Cursuri/1002 Things About Fox Pro/9/MCIConst.h
@@ -0,0 +1,100 @@
+
+*** Constant Group: ErrorConstants
+#define mciInvalidPropertyValue                           380
+#define mciGetNotSupported                                394
+#define mciSetNotSupported                                383
+#define mciInvalidProcedureCall                           5
+#define mciInvalidObjectUse                               425
+#define mciWrongClipboardFormat                           461
+#define mciDataObjectLocked                               672
+#define mciExpectedAnArgument                             673
+#define mciRecursiveOleDrag                               674
+#define mciFormatNotByteArray                             675
+#define mciDataNotSetForFormat                            676
+#define mciCantCreateButton                               30001
+#define mciCantCreateTimer                                30002
+#define mciUnsupportedFunction                            30004
+
+*** Constant Group: BorderStyleConstants
+#define mciNone                                           0
+#define mciFixedSingle                                    1
+
+*** Constant Group: RecordModeConstants
+#define mciRecordInsert                                   0
+#define mciRecordOverwrite                                1
+
+*** Constant Group: MousePointerConstants
+#define mciDefault                                        0
+#define mciArrow                                          1
+#define mciCross                                          2
+#define mciIBeam                                          3
+#define mciIcon                                           4
+#define mciSize                                           5
+#define mciSizeNESW                                       6
+#define mciSizeNS                                         7
+#define mciSizeNWSE                                       8
+#define mciSizeEW                                         9
+#define mciUpArrow                                        10
+#define mciHourglass                                      11
+#define mciNoDrop                                         12
+#define mciArrowHourglass                                 13
+#define mciArrowQuestion                                  14
+#define mciSizeAll                                        15
+#define mciCustom                                         99
+
+*** Constant Group: ModeConstants
+#define mciModeNotOpen                                    524
+#define mciModeStop                                       525
+#define mciModePlay                                       526
+#define mciModeRecord                                     527
+#define mciModeSeek                                       528
+#define mciModePause                                      529
+#define mciModeReady                                      530
+
+*** Constant Group: NotifyConstants
+#define mciNotifySuccessful                               1
+#define mciNotifySuperseded                               2
+#define mciAborted                                        4
+#define mciFailure                                        8
+
+*** Constant Group: OrientationConstants
+#define mciOrientHorz                                     0
+#define mciOrientVert                                     1
+
+*** Constant Group: FormatConstants
+#define mciFormatMilliseconds                             0
+#define mciFormatHms                                      1
+#define mciFormatMsf                                      2
+#define mciFormatFrames                                   3
+#define mciFormatSmpte24                                  4
+#define mciFormatSmpte25                                  5
+#define mciFormatSmpte30                                  6
+#define mciFormatSmpte30Drop                              7
+#define mciFormatBytes                                    8
+#define mciFormatSamples                                  9
+#define mciFormatTmsf                                     10
+
+*** Constant Group: OLEDropConstants
+#define mciOLEDropNone                                    0
+#define mciOLEDropManual                                  1
+
+*** Constant Group: DragOverConstants
+#define mciEnter                                          0
+#define mciLeave                                          1
+#define mciOver                                           2
+
+*** Constant Group: ClipBoardConstants
+#define mciCFText                                         1
+#define mciCFBitmap                                       2
+#define mciCFMetafile                                     3
+#define mciCFDIB                                          8
+#define mciCFPalette                                      9
+#define mciCFEMetafile                                    14
+#define mciCFFiles                                        15
+#define mciCFRTF                                          -16639
+
+*** Constant Group: OLEDropEffectConstants
+#define mciOLEDropEffectNone                              0
+#define mciOLEDropEffectCopy                              1
+#define mciOLEDropEffectMove                              2
+#define mciOLEDropEffectScroll                            -2147483648
