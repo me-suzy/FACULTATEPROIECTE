86c30a26f91428dbe8d94f13e5f97e28f11c68da
me-suzy <me.suzana@gmail.com>
2025-09-21 14:02:22 +0300

    Add Cursuri - Anul3 - FACUT WEB/Java/De la Strambei/UMLTranslating/Comanda.java

---
+++ Cursuri - Anul3 - FACUT WEB/Java/De la Strambei/UMLTranslating/Comanda.java
@@ -0,0 +1,19 @@
+/*
+ * Comanda.java
+ *
+ * Created on 19 mai 2003, 11:08
+ */
+
+package UMLTranslating;
+
+/**
+ *
+ * @author  strimbeic
+ */
+public final class Comanda {
+    
+    /** Creates a new instance of Comanda */
+    public Comanda() {
+    }
+    
+}
