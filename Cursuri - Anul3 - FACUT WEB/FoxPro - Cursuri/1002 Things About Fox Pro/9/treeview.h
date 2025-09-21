35eec61757861b4e55a58bdd037dd03cac2fe4c2
me-suzy <me.suzana@gmail.com>
2025-09-21 13:35:48 +0300

    Add Cursuri - Anul3 - FACUT WEB/FoxPro - Cursuri/1002 Things About Fox Pro/9/treeview.h

---
+++ Cursuri - Anul3 - FACUT WEB/FoxPro - Cursuri/1002 Things About Fox Pro/9/treeview.h
@@ -0,0 +1,45 @@
+* TreeLine Constants
+
+#define cntvwTreeLines						0
+*	Treelines shown
+#define cntvwRootLines						1
+*	Rootlines shown with Treelines
+
+* TreeRelationship Constants
+
+#define cntvwFirst							0
+*	First Sibling
+#define cntvwLast							1
+*	Last Sibling
+#define cntvwNext							2
+*	Next sibling
+#define cntvwPrevious						3
+*	Previous sibling
+#define cntvwChild							4
+*	Child
+
+* TreeStyle Constants
+
+#define cntvwTextOnly						0
+*	Text only
+#define cntvwPictureText					1
+*	Picture and text
+#define cntvwPlusMinusText					2
+*	Plus/minus and text
+#define cntvwPlusPictureText				3
+*	Plus/minus, picture, and text
+#define cntvwTreelinesText					4
+*	Treelines and text
+#define cntvwTreelinesPictureText			5
+*	Treelines, Picture, and Text
+#define cntvwTreelinesPlusMinusText			6
+*	Treelines, Plus/Minus, and Text
+#define cntvwTreelinesPlusMinusPictureText	7
+*	Treelines, Plus/Minus, Picture, and Text
+
+* LabelEdit Constants
+
+#define cntvwAutomatic						0
+*	Label Editing is automatic
+#define cntvwManual							1
+*	LabelEditing must be invoked
