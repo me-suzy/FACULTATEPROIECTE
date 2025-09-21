de1e06005c6a814fee75f6cb354a2508a814ea29
me-suzy <me.suzana@gmail.com>
2025-09-21 14:01:20 +0300

    Add Cursuri - Anul3 - FACUT WEB/Java/De la Strambei/salariati/Departament.java

---
+++ Cursuri - Anul3 - FACUT WEB/Java/De la Strambei/salariati/Departament.java
@@ -0,0 +1,16 @@
+/*
+ * Departament.java
+ *
+ * Created on 17 martie 2003, 11:29
+ */
+
+package salariati;
+
+/**
+ *
+ * @author  strimbeic
+ */
+public class Departament {
+  public String nume;
+  public int nrMembri;
+}
