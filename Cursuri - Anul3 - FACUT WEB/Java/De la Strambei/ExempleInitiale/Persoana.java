44826c25a6fe9da4a5900734dd41bb3e313996b3
me-suzy <me.suzana@gmail.com>
2025-09-21 13:54:17 +0300

    Add Cursuri - Anul3 - FACUT WEB/Java/De la Strambei/ExempleInitiale/Persoana.java

---
+++ Cursuri - Anul3 - FACUT WEB/Java/De la Strambei/ExempleInitiale/Persoana.java
@@ -0,0 +1,23 @@
+/*
+ * Persoana.java
+ *
+ * Created on 02 martie 2004, 19:35
+ */
+
+package ExempleInitiale;
+
+/**
+ *
+ * @author  strimbeic
+ */
+/** Persoana.java */
+
+public class Persoana extends Object{ // definitie clasa Persoana
+// membri-variabile publici
+  public String cnp;
+  public String numePren;
+  public String domiciliu;
+
+  protected Persoana(){/*constructorul clasei persoana*/}
+} // sfarsit definitie clasa Persoana
+
